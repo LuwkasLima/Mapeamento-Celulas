abafcd29c814eb4c0cf874fa7e14bdc37363f471
Lucas Lima <lucas.m.s.lima@gmail.com>
2013-11-15 21:44:13 -0200

    Included the "Grupos" folder to receive the files related to Custom Post Types

---
+++ grupos/metaboxes_grupos.php
@@ -0,0 +1,39 @@
+<div class="my_meta_control">
+	
+	    
+    <div id="campo_meta">
+    <label>Site do Grupo</label>
+    <p>
+		<input type="text" name="_my_meta[ag_site]" value="<?php if(!empty($meta['ag_site'])) echo $meta['ag_site']; ?>"/>
+		<span>Coloque o endere&ccedil;o (URL) do site (com http://)</span>
+	</p>
+    </div>
+    
+	<div id="campo_meta">
+    <label>Grupo no FaceBook</label>
+	<p>
+		<input type="text" name="_my_meta[ag_face]" value="<?php if(!empty($meta['ag_face'])) echo $meta['ag_face']; ?>"/>
+		<span>Coloque o endere&ccedil;o (URL) no FaceBook (com http://)</span>
+	</p>
+    </div>
+	
+	<div id="campo_meta">
+    <label>E-mail Oficial</label>
+	<p>
+		<input type="text" name="_my_meta[ag_email]" value="<?php if(!empty($meta['ag_email'])) echo $meta['ag_email']; ?>"/>
+		<span>Coloque o e-mail de contato do Grupo ou Nação</span>
+	</p>
+    </div>
+	
+	
+	<div id="campo_meta">
+    <label>Integrantes</label>
+	<p>
+		<input type="text" name="_my_meta[ag_integrantes]" value="<?php if(!empty($meta['ag_integrantes'])) echo $meta['ag_integrantes']; ?>"/>
+		<span>Coloque a quantidade atual de integrantes do Grupo ou Nação</span>
+	</p>
+    </div>
+	
+    <div id="hack-meta"></div>  
+	
+</div>
